36ed0338642aaf0b8e980225938bfe4f4529ef53
James Moore <jobpush9672@gmail.com>
2025-12-03 20:14:42 +0000

    Add repo structure with scripts, logs, config, tests, docs

---
+++ scripts/test.py
@@ -0,0 +1,10 @@
+import datetime
+
+name = "James Moore"
+email = "jobpush9672@gmail.com"
+github_username = "jbmoore9672"
+
+print(f"Hello {name}, Python is working!")
+print(f"Your GitHub username is: {github_username}")
+print(f"Your email is: {email}")
+print("Current time:", datetime.datetime.now())
